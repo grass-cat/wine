6ce69427d0f4966c92aff1d658aac9eb9cd0ae0e
grass-cat <130976659+grass-cat@users.noreply.github.com>
2023-04-22 14:16:10 +0700

    Update peazip.sh

--- peazip.sh
+++ peazip.sh
@@ -7,6 +7,10 @@ wget https://github.com/peazip/PeaZip/releases/download/9.1.0/peazip_9.1.0.LINUX
 sudo apt install ./peazip_9.1.0.LINUX.GTK2-1_amd64.deb
 
 # DEL DEB
-sudo su
+cd
 
 rm peazip_9.1.0.LINUX.GTK2-1_amd64.deb
+
+clean
+
+cd
